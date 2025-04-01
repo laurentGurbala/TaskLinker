5faa1431b9ef03b1102876192cfe823e4449fff3
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-04-01 17:00:24 +0200

    feat(accueil): Récupération de la liste des projets

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ProjectRepository $repository): Response
     {
+        // Récupère les projets
+        $projects = $repository->findAll();
+
+        // Affiche la page d'acceuil avec la liste des projets
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'projects' => $projects,
         ]);
     }
 }
