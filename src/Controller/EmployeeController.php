9d23eed05a0ef4d05655dbd7f56c5e5dc00f6b08
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-04-03 13:41:00 +0200

    feat(employee): Edition d'un employé

---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employee;
+use App\Form\EmployeeType;
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EmployeeController extends AbstractController
+{
+    #[Route('/team', name: 'app_employee_index')]
+    public function index(EmployeeRepository $employeeRepository): Response
+    {
+        // Récupérer les employés
+        $employees = $employeeRepository->findAll();
+
+        return $this->render('employee/index.html.twig', [
+            "employees" => $employees,
+        ]);
+    }
+
+    #[Route("/team/{id}/edit", name: "app_employee_edit", methods: ["GET", "POST"], requirements: ["id" => "\d+"])]
+    public function edit(?Employee $employee, Request $request, EntityManagerInterface $entityManager): Response {
+        // Si l'employee n'existe pas...
+        if (!$employee) {
+            // Rediriger vers la page d'équipe
+            return $this->redirectToRoute("app_employee_index");
+        }
+
+        // Crée le formulaire
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        // Vérifie si le formulaire à été soumis et est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Enregistre le nouveau projet en BDD
+            $entityManager->persist($employee);
+            $entityManager->flush();
+
+            // Redirige vers la liste des employées
+            return $this->redirectToRoute("app_employee_index");
+        }
+
+        // Affiche la page d'édition d'un employé
+        return $this->render("employee/new.html.twig", [
+            "form" => $form,
+            "employee" => $employee
+        ]);
+    }
+}
